1557f24b73720d97f0e5f4ee624c4dd9046446de
beranrdn38 <erisboxx@gmail.com>
2022-05-19 13:49:07 -0500

    add validation to api requests

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,10 +2,22 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private Long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    @NotEmpty(message = "body should not be null or empty")
+    @Size(min=19, message = "Body should be at least 10 characters")
     private String body;
 }
